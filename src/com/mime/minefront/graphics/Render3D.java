3ce9f43222848a20b64e47a39e0d5bd17ff850fe
emrecellebi <Emre_celebi_199505@hotmail.com>
2021-05-14 13:21:26 +0300

    Floors and Animation

--- src/com/mime/minefront/graphics/Render3D.java
+++ src/com/mime/minefront/graphics/Render3D.java
@@ -2,6 +2,8 @@ package com.mime.minefront.graphics;
 
 public class Render3D extends Render
 {
+	double time = 0;
+	
 	public Render3D(int width, int height)
 	{
 		super(width, height);
@@ -11,15 +13,24 @@ public class Render3D extends Render
 	{
 		for(int y = 0; y < height; y++)
 		{
-			double yDepth = y - height / 2.4;
-			double z = 100.0 / yDepth;
+			double ceiling = (y - height / 2.0) / height;
+			
+			if(ceiling < 0)
+				ceiling = -ceiling;
+			
+			double z = 8 / ceiling;
+			
+			time += 0.0005;
 			
 			for(int x = 0; x < width; x++)
 			{
-				double depth = x - width / 2;
+				double depth = (x - width / 2.0) / height;
 				depth *= z;
-				int xx = (int)depth & 5;
-				pixels[x + y * width] = xx * 128;
+				double xx = depth;
+				double yy = z + time;
+				int xPix = (int)(xx);
+				int yPix = (int)(yy);
+				pixels[x + y * width] = ((xPix & 15) * 16) | ((yPix & 15) * 16) << 8;
 			}
 		}
 	}
